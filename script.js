280a99bd53ef9c48ad9f9e0bd0e237ea8481a740
dipen2000 <dipenchavda2000@gmail.com>
2021-11-29 03:15:35 +0530

    app finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const initialInput = document.querySelector("#initial-input");
 const quantityInput = document.querySelector("#quantity-input");
 const currentInput = document.querySelector("#current-input");
 const btn = document.querySelector("#btn");
-const message = document.querySelector("#message");
+const message = document.querySelector(".message");
 
 btn.addEventListener("click" , clickHandler);
 
@@ -16,21 +16,30 @@ function clickHandler(){
 }
 
 function calculateProfitLoss(initial,quantity,current){
-    if(initial > current){
-        let loss = (initial-current)*quantity;
-        let lossPercentage = (loss / initial) * 100;
-        displayMessage(`Hey the loss is ${loss} and percentage is ${lossPercentage}%`);
-    }
-    else if(current > initial){
-        let profit = (current - initial)*quantity;
-        let profitPercentage = (profit/initial)*100;
-        displayMessage(`Hey the profit is ${profit} and the percentage is ${profitPercentage}`);
+    if(initial === 0 || quantity === 0 || current === 0){
+        message.innerText = "Please fill the fields correctly."
     }
     else{
-        displayMessage("No pain , no gain and no gain , no pain.");
+        if(initial > current){
+            let loss = ((initial-current)*quantity).toFixed(2);
+            let lossPercentage = ((loss / initial) * 100).toFixed(2);
+            // message.classList.add("loss");
+            displayMessage(`Hey the loss is ${loss} and percentage is ${lossPercentage}%` , "loss");
+        }
+        else if(current > initial){
+            let profit = ((current - initial)*quantity).toFixed(2);
+            let profitPercentage = ((profit/initial)*100).toFixed(2);
+            // message.classList.add("profit");
+            displayMessage(`Hey the profit is ${profit} and the percentage is ${profitPercentage}%` , "profit");
+        }
+        else{
+            // message.classList.add("neutral");
+            displayMessage("No pain , no gain and no gain , no pain." , "neutral");
+        }
     }
+    
 }
 
-function displayMessage(msg){
-    message.innerText = msg;
+function displayMessage(msg , className){
+    message.innerHTML = `<p class="${"message " + className}">${msg}</p>`;
 }
